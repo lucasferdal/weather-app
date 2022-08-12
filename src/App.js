a056f843f14a66768ce256a118d8d5506f2d3192
lucasferdal <electroblue25@gmail.com>
2022-08-12 18:43:30 -0300

    Segundo commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import './App.css';
+import Region from './components/Region.js';
 
 const App = () => (
   <div>
-    <h1>Buen dia grupoo</h1>
+
+    <header>
+      <h1>Como esta el tiempo?</h1>
+    </header>
+
+    <div className='buscarRegion'>
+      <input types="button" id='buscar' placeholder='Busca tu ciudad'/>
+      <input type="submit" id='enviar' />
+    </div>
+
+      <h2>Asi se vera el tiempo en los proximos dias</h2>
+      
+    <div className='regiones'>
+
+      <Region>
+      </Region>
+      
+      <Region>
+      </Region>
+
+      <Region>
+      </Region>
+
+    </div>
+
   </div>
 )
 
---
+++ src/components/Region.js
@@ -0,0 +1,10 @@
+import React from "react";
+import '../styles/Region.css';
+
+const Region = () => (
+    <div className="Region">
+        {/* <h2>Este es el tiempo en tu region</h2> */}
+    </div>
+)
+
+export default Region;
